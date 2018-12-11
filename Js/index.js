59c3d3c27256bc2c7eb7c9a03dce5669a1f7986d
ThanhTung1235 <tungltd00602@fpt.edu.vn>
2018-12-11 15:26:39 +0700

    add mobile menu

--- Js/index.js
+++ Js/index.js
@@ -1,6 +1,10 @@
 var btn_bars = document.getElementById("btn-bars");
 var menu_respon = document.getElementById("menu-respon");
 var lst = document.getElementsByClassName("nav-mobile")[0];
+var people = document.getElementsByClassName("people")[0];
+var discover = document.getElementsByClassName("discover")[0];
+var tvShow = document.getElementsByClassName("tv")[0];
+var movie = document.getElementsByClassName("movie")[0];
 btn_bars.onclick = function () {
     if (menu_respon.classList.contains("active") == false) {
         menu_respon.classList.add("active");
@@ -10,3 +14,32 @@ btn_bars.onclick = function () {
         lst.classList.add("d-none");
     }
 };
+
+function Discover() {
+    if (discover.classList.contains("d-none")) {
+        discover.classList.remove("d-none");
+    }
+    else {
+        discover.classList.add("d-none");
+    }
+}
+function Movies() {
+    if (movie.classList.contains("d-none")) {
+        movie.classList.remove("d-none");
+    }
+    else {
+        movie.classList.add("d-none");
+    }
+}
+function TvShow() {
+    if (tvShow.classList.contains("d-none")) {
+        tvShow.classList.remove("d-none");
+    }
+    else {
+        tvShow.classList.add("d-none");
+    }
+}
+function People() {
+    if (people.classList.contains("d-none"))people.classList.remove("d-none");
+    else people.classList.add("d-none");
+}
