e0360444ef25228dce6cd4040a622e0e82497cb2
ThanhTung1235 <tungltd00602@fpt.edu.vn>
2018-12-13 16:14:25 +0700

    done people

--- Js/People/details.js
+++ Js/People/details.js
@@ -10,8 +10,8 @@ document.addEventListener('DOMContentLoaded', function () {
 function Person(id) {
     var api_key = "f6f0ae796cca1a731af364386893d5fe";
     var url = "https://api.themoviedb.org/3/person/" + id + "?api_key=" + api_key;
-    var thumbnail = document.getElementById("thumbnail_person");
-    var content = document.getElementById("content_detail");
+    var person = document.getElementById("person");
+    var personal_info = document.getElementById("personal_info");
     var xmlHttpRequest = new XMLHttpRequest();
     xmlHttpRequest.onreadystatechange = function () {
         if (this.readyState == 4 && this.status == 200) {
@@ -25,7 +25,7 @@ function Person(id) {
                 0: "-"
             };
             console.log(gender[Person.gender]);
-            var item = '<img class="img-fluid" src="https://image.tmdb.org/t/p/w185_and_h278_bestv2/' + Person.profile_path + '" alt="">';
+            var item = '<div class="container">';
             item += '<h2 class="detail-item">' + Person.name + '</h2>';
             item += '<ul class="detail-item">';
             item += '<li class="item-d"><b>Known For</b></li>';
@@ -56,13 +56,28 @@ function Person(id) {
             for (var i = 0; i < Person.also_known_as.length; i++) {
                 item += '<li class="item-d">' + Person.also_known_as[i] + '</li>';
             }
-            thumbnail.innerHTML += item;
-            content_detail.innerHTML = '<h2 class="detail-title">'+Person.name+'</h2>\n' +
-                '                <div class="detail-info">\n' +
-                '                    <p>Biography</p>\n' +
-                '                    <p class="biography">'+Person.biography+'</p>\n' +
-                '                    <p class="btn-more"><i class="fas fa-chevron-down"></i></p>\n' +
-                '                </div>';
+            item += '</ul>';
+            item += '</div>'
+            personal_info.innerHTML += item;
+
+            var personItem = '';
+            personItem += '<div class="detail-thumbnail">';
+            personItem += '<div class="person-avatar">';
+            personItem += '<img class="img-fluid" src="https://image.tmdb.org/t/p/w185_and_h278_bestv2/' + Person.profile_path + '" alt="">';
+            personItem += '</div>';
+            personItem += '</div>';
+            personItem += '<div class="detail">';
+            personItem += '<div class="detail-info">';
+            personItem += '<h2 class="detail-title"> ' + Person.name + '</h2>';
+            personItem += '<p class="annotation">Biography</p>';
+            personItem += '<div class="biography">' + Person.biography + '</div>';
+            personItem += '<p class="btn-more d-hidden"><i class="fas fa-chevron-down"></i></p>';
+            personItem += '</div>';
+            personItem += '</div>';
+            personItem += '</div>';
+            person.innerHTML += personItem;
+
+
         }
     };
     xmlHttpRequest.open('GET', url, true);
